540be5ac1aa76d1ac52094edb78bbe9b59d4f03d
Feyi AbrahamAdesanya <val.ezeh15@gmail.com>
2018-06-17 16:55:56 +0100

    Feature (get all rides routes api endpoint): get all rides routes api end point  implemented - set get all rides route in routes/index [Deliver #158378293]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
-
+import AllRides  from '../dummy_data/controllers/getAllRides'
 const router = express.Router();
 
 // defines routes
 router.get('/', (req, res) => {
     res.send('Hello, Welcome to Ride-My-Way App');
 });
+
+// routes for ride-my-way
+router.get('/rides', AllRides.getAllRides);
 
 // catch all invalid routes
 router.get('*', (req, res) => res.status(404).json({
