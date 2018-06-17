ead089317f5a5c0c230d815b54e79ef7094817bd
Feyi AbrahamAdesanya <val.ezeh15@gmail.com>
2018-06-17 15:57:12 +0100

    - set up object in db.js in model [Finishes #158412999]

---
+++ server/dummy_data/model/db.js
@@ -0,0 +1,52 @@
+const ride = [
+    {
+        id: 1,
+        name: 'Ajayi Samuel',
+        from: 'ccHub, Herbert Macaulay Road, Lagos Mainland',
+        to: 'Adetokunbo Ademola Street, Eti-Osa',
+        departure_time: '4pm',
+        ride_request: [],
+    },
+    {
+        id: 2,
+        name: 'Samuel Tutu',
+        from: 'CMS, Lagos Island',
+        to: 'Adetokunbo Ademola Street, Eti-Osa',
+        departure_time: '5pm',
+        ride_request: [],
+    },
+    {
+        id: 3,
+        name: 'Ezeh Chika',
+        from: 'Gwarinpa Estate, Abuja, Federal Capital Territory, Nigeria',
+        to: 'Danube Street, Abuja, Federal Capital Territory, Nigeria',
+        departure_time: '2pm',
+        ride_request: [],
+    },
+    {
+        id: 4,
+        name: 'Jennifer Dickson',
+        from: 'Herbert Macaulay Road, Lagos Mainland',
+        to: 'Amuwo Odofin',
+        departure_time: '8pm',
+        ride_request: [],
+    },
+    {
+        id: 5,
+        name: 'Beulah Isaac',
+        from: 'Campus Mini Stadium, Lagos Island',
+        to: 'Victoria Island, Eti-Osa',
+        departure_time: '3pm',
+        ride_request: [],
+    },
+    {
+        id: 6,
+        name: 'Jeffery Owolabi',
+        from: 'Ikoyi Club 1938 Road, Lagos Island',
+        to: 'Omole Bus Stop, Isheri Road, Ikeja',
+        departure_time: '7pm',
+        ride_request: [],
+    },
+];
+
+export default ride;
