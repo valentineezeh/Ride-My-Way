b6379e7da414a228bb7c6e6fa270b77d7657f382
valentineezeh <32917511+valentineezeh@users.noreply.github.com>
2018-06-30 19:57:54 +0100

    chore(test for user api):signup and login (#40)

--- server/middleware/signInValidator.js
+++ server/middleware/signInValidator.js
@@ -7,35 +7,35 @@ class SignInValidation{
             errors.push('Email should not be empty');
             return res.status(400).send({
                 status: 'Error',
-                msg: errors
+                message: errors
             });
         }
         if (!validator.isEmail(req.body.email)){
             errors.push('Email must be valid..');
             return res.status(400).send({
                 status: 'Error',
-                msg: errors
+                message: errors
             });
         }
         if (req.body.email == undefined){
             errors.push('Email is required..');
             return res.status(400).send({
                 status: 'Error',
-                msg: errors
+                message: errors
             });
         }
         if ( req.body.password == undefined){
             errors.push('Password is required...');
             return res.status(400).send({
                 status: 'Error',
-                msg: errors
+                message: errors
             });
         }
         if ( req.body.password == ''){
             errors.push('Password should not be empty.');
             return res.status(400).send({
                 status: 'Error',
-                msg: errors
+                message: errors
             });
         }
         return next();
