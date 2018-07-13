b9389ac0172c76c4e5d4392c0f5957aa74d27cff
Feyi AbrahamAdesanya <val.ezeh15@gmail.com>
2018-07-13 15:36:31 +0100

    ft(UI)user login feature - Implement login feature for user via UI [Deliver #158377660]

--- UI/js/login.js
+++ UI/js/login.js
@@ -1,9 +1,58 @@
+var url = 'http://localhost:3000/api/v1/auth/login';
+
 // Get the modal
 var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = 'none';
     }
+};
+
+var loginBtn = document.getElementById('loginBtn');
+loginBtn.addEventListener('click', loginUser);
+
+function loginUser (e) {
+    e.preventDefault();
+    var email = document.getElementById('userLoginEmail').value;
+    console.log(email);
+    var password = document.getElementById('userLoginPassword').value;
+    console.log(password);
+
+    var loginDetail = {
+        email: email,
+        password: password
+    };
+
+    var fetchLoginData = {
+        method: 'POST',
+        mode: 'cors',
+        body: JSON.stringify(loginDetail),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            Accept: 'application/json, text/plain, */*',
+            'Content-type': 'application/json',
+        }
+    };
+
+    fetch(url, fetchLoginData)
+        .then((res) => {
+            // console.log(res)
+            return res.json();
+        })
+        .then((user) => {
+            if(user.success == true && user.data && user.message == 'Welcome User You are now Logged In' && user.data.token){
+                window.sessionStorage.setItem('token', user.data.token);
+                window.location = 'myRide.html';
+            }
+            // console.log(user)
+            // var token = user.data.token;
+            // console.log(token);
+            
+            // return data
+        }).catch((err) => {
+            return err
+        })
+    document.getElementById('loginForm').reset();
 }
--- UI/js/signup.js
+++ UI/js/signup.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:3000/api/v1/auth/signup';
+var signUpurl = 'http://localhost:3000/api/v1/auth/signup';
 
 // Get the modal
 var modal = document.getElementById('id02');
@@ -10,53 +10,58 @@ window.onclick = function(event) {
     }
 };
 
-const btn = document.querySelector('#signUpBtn');
+var btn = document.querySelector('#signUpBtn');
 btn.addEventListener('click', saveUser);
 
-const saveUser = (e) => {
+function saveUser (e) {
     e.preventDefault();
 
-    let firstname = document.getElementById('firstname').value;
-    console.log(firstname);
-    let lastname = document.getElementById('lastname').value;
-    console.log(lastname);
-    let bio = document.getElementById('about').value;
-    console.log(bio);
-    let email = document.getElementById('userEmail').value;
-    console.log(email);
-    let password = document.getElementById('userPassword').value;
-    let confirmPassword = document.getElementById('confirmPassword').value;
+    var firstname = document.getElementById('firstname').value;
+    var lastname = document.getElementById('lastname').value;
+    var about = document.getElementById('about').value;
+    var email = document.getElementById('userEmail').value;
+    var password = document.getElementById('userPassword').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
 
-    let userDetail = {
+    console.log(firstname, lastname, about, email, password, confirmPassword);
+
+    var userDetail = {
         firstname: firstname,
         lastname: lastname,
-        bio: bio,
+        about: about,
         email: email,
         password: password,
         confirmPassword: confirmPassword
     };
 
-    let fetchData = {
+    var fetchData = {
         method: 'POST',
-        body: JSON.stringify({
-            user: { userDetail }
-        }),
+        mode: 'cors',
+        body: JSON.stringify(userDetail),
         headers: {
+            'Access-Control-Allow-Origin': '*',
             Accept: 'application/json, text/plain, */*',
-            'Content-type': 'application/json',
-            'Authorization': token
+            'Content-type': 'application/json',   
         }
     };
 
-    fetch(url, fetchData)
+    fetch(signUpurl, fetchData)
         .then((res) => {
+            console.log(res);
             return res.json();
         })
         .then((data) => {
+            var token = data.token;
+            console.log('token for user ' + token)
             console.log(data);
+            if(data.data && data.message == 'User registration successful' && data.token){
+                window.sessionStorage.setItem('token', data.token);
+                window.location = 'myRide.html';
+            }
+            return data;
         }).catch((err) => {
-            console.log.err;
+            return err;
         });
 
     document.getElementById('signUpForm').reset();
-};
+}
