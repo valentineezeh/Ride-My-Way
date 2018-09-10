f78dfda080ae2515e9197a4bdbc13ec401d42a66
valentineezeh <32917511+valentineezeh@users.noreply.github.com>
2018-09-10 15:42:47 +0100

    feature(landing page): implement landing page (#58)
    - Implement HomePage component
    - Implement FootBar component
    [Finishes #160385887]

--- client/component/App.jsx
+++ client/component/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import NavigationBar from './NavigationBar.jsx';
-
+import HomePage from '../component/home/HomePage.jsx';
+import FootBar from '../component/footer/FootBar.jsx'
 class App extends React.Component {
   render(){
     return (
       <div className="container">
         <NavigationBar />
+        <div>
+          <Route exact path='/' component={HomePage} />
+        </div>
+        <FootBar />
       </div>
     );
   }
